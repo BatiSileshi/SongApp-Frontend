f3ee4b12b8af73e8faccb341a4c96838c0111841
jiosaa <batisileshi93@gmail.com>
2023-02-28 16:11:54 -0500

    now you can add song

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
@@ -12,6 +12,11 @@ import SongData from './data/SongData';
 function App() { 
   const [song, setSong] = useState(SongData)
 
+  const addSong = (newSong) => {
+    newSong.id = uuidv4()
+    setSong([newSong, ...song])
+  }
+
   const deleteSong = (id) => {
     if(window.confirm('Are you sure you want to delete?')){
       setSong(song.filter((item) => item.id !== id));
@@ -23,7 +28,7 @@ function App() {
     <Header />  
       <div className="song">
        {/* passing song props to SongList  - goto SongList*/}
-       <SongForm />
+       <SongForm handleAdd={addSong}/>
        <SongStats song={song}/>
       <SongList song={song} handleDelete={deleteSong}/>
         
--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -1,15 +1,15 @@
 import { useState } from "react"
 
-function SongForm() {
-    const [title, setTitle]=useState('')
+function SongForm({handleAdd}) {
+    const [song_title, setTitle]=useState('')
     const [album, setAlbum]=useState('')
-    const [artist, setArtist]=useState('')
+    const [artist_name, setArtist]=useState('')
 
     const [btnDisabled, setBtnDisabled]=useState(true)
     const [message, setMessage]=useState()
 
     const handleSongTitleChange = (e)=> {
-        if (title === ''){
+        if (song_title === ''){
             setBtnDisabled(true)
             setMessage(null)
 
@@ -21,22 +21,56 @@ function SongForm() {
     }
 
     const handleSongAlbumChange = (e)=> {
+        if (album === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setAlbum(e.target.value)
     }
 
     const handleSongArtistChange = (e)=> {
+        if (artist_name === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setArtist(e.target.value)
+    }
+
+    const handleSubmit=(e) => {
+      e.preventDefault()
+      if (song_title !== ''){
+        const newSong = {
+          song_title,
+          album,
+          artist_name
+
+        }
+        handleAdd(newSong)
+
+        setTitle('')
+        setAlbum('')
+        setArtist('')
+      }
+       
     }
 
   return (
     <div className='song'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-            <input onChange={handleSongTitleChange} type="text" name="" placeholder='Song title' value={title}/>
+            <input onChange={handleSongTitleChange} type="text" name="" placeholder='Song title' value={song_title}/>
 
             <input onChange={handleSongAlbumChange} type="text" name="" placeholder='Song album' value={album}/>
 
-            <input onChange={handleSongArtistChange} type="text" name="" placeholder='Artist name' value={artist} />
+            <input onChange={handleSongArtistChange} type="text" name="" placeholder='Artist name' value={artist_name} />
 
             <button type="submit" isdisabled={btnDisabled}>Save</button>
         </div>
